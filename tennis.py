722157c0a6db737d6023f9a94911a88e27b682bc
Anas Alkhatib <68076293+alkhatib-shopify@users.noreply.github.com>
2022-01-19 18:55:05 +0000

    extract the body of the if/else statement in score into its own methods for clarity

--- tennis.py
+++ tennis.py
@@ -22,31 +22,32 @@ class TennisGame1:
     
     def game_tied(self):
       return self.p1points == self.p2points
+
+    def game_tied_result(self):
+      return {
+        0 : "Love-All",
+        1 : "Fifteen-All",
+        2 : "Thirty-All",
+      }.get(self.p1points, "Deuce")
     
     def advantage_or_win(self):
       return self.p1points>=4 or self.p2points>=4
-        
 
-    def score(self):
-        result = ""
-        if self.game_tied():
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.advantage_or_win():
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result = f"Advantage {self.player1Name}"
-            elif (minusResult ==-1):
-                result = f"Advantage {self.player2Name}"
-            elif (minusResult>=2):
-                result = f"Win for {self.player1Name}"
-            else:
-                result = f"Win for {self.player2Name}"
-        else:
-            for i in range(1,3):
+    def advantage_or_win_result(self):
+      minusResult = self.p1points-self.p2points
+      if (minusResult==1):
+          result = f"Advantage {self.player1Name}"
+      elif (minusResult ==-1):
+          result = f"Advantage {self.player2Name}"
+      elif (minusResult>=2):
+          result = f"Win for {self.player1Name}"
+      else:
+          result = f"Win for {self.player2Name}"
+      return result
+    
+    def on_going(self):
+      result = ""
+      for i in range(1,3):
                 if (i==1):
                     tempScore = self.p1points
                 else:
@@ -58,6 +59,16 @@ class TennisGame1:
                     2 : "Thirty",
                     3 : "Forty",
                 }[tempScore]
+      return result
+
+    def score(self):
+        result = ""
+        if self.game_tied():
+            result = self.game_tied_result()
+        elif self.advantage_or_win():
+            result = self.advantage_or_win_result()
+        else:
+            result = self.on_going()
         return result
 
 
